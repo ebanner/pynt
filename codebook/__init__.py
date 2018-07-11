374621d911aa02d34b73291738eb2c1ccb4a5ada
Edward Banner <edward.banner@gmail.com>
2018-07-10 17:29:18 -0700

    Get debugger driven development feature working

--- codebook/__init__.py
+++ codebook/__init__.py
@@ -1,1 +1,33 @@
+import inspect
+import logging
+import traceback
+
+from IPython.core.ultratb import AutoFormattedTB
+
 from .manager import ExternalIPythonKernelManager
+
+
+def handle_exception(type, value, tb):
+    # Print stack trace.
+    info = traceback.format_exception(type, value, tb)
+    logging.error(''.join(info))
+
+    # Get local variables.
+    while True:
+        if not tb.tb_next:
+            break
+        tb = tb.tb_next
+    local_vars = tb.tb_frame.f_locals
+
+    # Start kernel.
+    import os
+    pid = os.fork()
+    if pid == 0:
+        open(f"{os.environ['HOME']}/.pynt", 'a').close()
+        import IPython
+        IPython.start_kernel(user_ns=local_vars)
+    os.waitpid(pid, 0)
+
+def register():
+    import sys
+    sys.excepthook = handle_exception
